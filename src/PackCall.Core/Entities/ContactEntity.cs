0fc4e60d66ec4dbe158e8bb57eee205db03c92a9
Georgeiy <gv-it-lab@yandex.ru>
2026-07-24 15:58:59 +0300

    -

--- src/PackCall.Core/Entities/ContactEntity.cs
+++ src/PackCall.Core/Entities/ContactEntity.cs
@@ -1,19 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PackCall.Core.Entities
+﻿namespace PackCall.Core.Entities
 {
     internal class ContactEntity
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
 
-        public IReadOnlyDictionary<ContactType, string> Type { get; set; }
+        public IReadOnlyDictionary<ContactType, string> Channels { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; }
 
         public enum ContactType
         {
             Email
+        }
+
+        public ContactEntity(string name, IReadOnlyDictionary<ContactType, string> channels, DateTimeOffset createdAt)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Channels = channels;
+            CreatedAt = DateTime.Now;
         }
     }
 }
--- src/PackCall.Core/Entities/DeliveryEntity.cs
+++ src/PackCall.Core/Entities/DeliveryEntity.cs
@@ -6,7 +6,18 @@ namespace PackCall.Core.Entities
 {
     internal class DeliveryEntity
     {
-        
+        public Guid Id { get; set; }
+
+        public Guid CampaignId { get; set; }
+
+        public Guid ContactId { get; set; }
+
+        public string ContactSnapshot { get; set; }
+
+        public enum DeliveryChannel
+        {
+            Email
+        }
 
     }
 }
